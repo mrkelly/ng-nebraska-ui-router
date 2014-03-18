74eb688ac059e3ab5aee554da4cfb0ffa4e7afce
Michael Kelly <michael@mikeryankelly.com>
2014-03-17 21:13:35 -0500

    Added data to states.

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -5,22 +5,35 @@ angular.module('app', [
     $stateProvider
       .state('app', {
         controller: 'AppCtrl',
-        templateUrl: 'assets/templates/app/index.html'
+        templateUrl: 'assets/templates/app/index.html',
+        data: {
+          appTitle: 'Angular UI Router'
+        }
       })
         .state('app.state1', {
           url: '/state1',
           controller: 'State1Ctrl',
-          templateUrl: 'assets/templates/app/state1.html'
+          templateUrl: 'assets/templates/app/state1.html',
+          data : {
+            stateTitle: 'State 1'
+          }
         })
         .state('app.state2', {
           url: '/state2',
           controller: 'State2Ctrl',
-          templateUrl: 'assets/templates/app/state2.html'
+          templateUrl: 'assets/templates/app/state2.html',
+          data : {
+            stateTitle: 'State 2'
+          }
         })
         .state('app.state3', {
           url: '/state3',
           controller: 'State3Ctrl',
-          templateUrl: 'assets/templates/app/state3.html'
+          templateUrl: 'assets/templates/app/state3.html',
+          data : {
+            appTitle: 'Angular UI Router (The Real Deal)',
+            stateTitle: 'State 3'
+          }
         });
 
 
--- client/src/app/controllers.js
+++ client/src/app/controllers.js
@@ -1,16 +1,13 @@
 angular.module('app.controllers', [ ])
-  .controller('AppCtrl', function ($scope){
-    $scope.state3Content = "We have nested scope!";
+  .controller('AppCtrl', function ($scope, $state){
+
   })
   .controller('State1Ctrl', function ($scope){
-    $scope.title = "State 1";
-    $scope.content = "Now we have 3 children states and one parent state";
+
   })
   .controller('State2Ctrl', function ($scope){
-    $scope.title = "State 2";
-    $scope.content = "We've removed the nav duplication";
+
   })
   .controller('State3Ctrl', function ($scope){
-    $scope.title = "State 3";
-    $scope.content = $scope.state3Content;
+
   });
